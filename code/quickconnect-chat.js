2e9e9e00ed5bc76752fa030d8f297f8a5356ec9c
Silvia Pfeiffer <spfeiffer@nicta.com.au>
2014-08-08 12:49:21 +1000

    Remove quickconnect-chat - it is already explained in simple-text-share.

--- code/quickconnect-chat.js
+++
@@ -1,7 +0,0 @@
-var quickconnect = require('rtc-quickconnect');
-
-quickconnect('//switchboard.rtc.io/', { room: 'chat-tutorial' })
-  .createDataChannel('chat')
-  .on('channel:opened:chat', function(id, dc) {
-    console.log('a data channel has been opened to peer: ' + id);
-  });
